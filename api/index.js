8253d009661d407176789127a1be962bd6b04240
GoldenEy3-maker <danil-danil-korolev@bk.ru>
2024-12-30 15:16:37 +0700

    feat: add static assets

--- api/index.js
+++ api/index.js
@@ -1,20 +1,9 @@
 import express from "express";
-import { readFile } from "fs";
 import { generateScalarUI } from "openapi-metadata/ui";
 
 const app = express();
+app.use(express.static("public"));
 const port = 8080;
-
-app.get("/schema", (req, res) => {
-  readFile("/api-schema.json", (err, data) => {
-    if (err) {
-      res.status(500).send({ error: "Failed to read API schema file" });
-      return;
-    }
-
-    res.header("Content-Type", "application/json").send(data);
-  });
-});
 
 app.get("/", (req, res) => {
   const ui = generateScalarUI("/api-schema.json");
@@ -22,7 +11,7 @@ app.get("/", (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`Open Api listening on port ${port}`);
 });
 
 export default app;
