97f782037808247827acb6c9d70a6e001e85d718
GoldenEy3-maker <danil-danil-korolev@bk.ru>
2024-12-30 14:59:45 +0700

    feat: init

---
+++ api/index.js
@@ -0,0 +1,28 @@
+import express from "express";
+import { readFile } from "fs";
+import { generateScalarUI } from "openapi-metadata/ui";
+
+const app = express();
+const port = 8080;
+
+app.get("/schema", (req, res) => {
+  readFile("api-schema.json", (err, data) => {
+    if (err) {
+      res.status(500).send({ error: "Failed to read API schema file" });
+      return;
+    }
+
+    res.header("Content-Type", "application/json").send(data);
+  });
+});
+
+app.get("/", (req, res) => {
+  const ui = generateScalarUI("/schema");
+  res.type("html").send(ui);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
+
+export default app;
